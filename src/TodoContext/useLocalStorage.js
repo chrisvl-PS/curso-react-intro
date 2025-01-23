cb5619d241adb746002eb42c956ed314a5920fb9
chrisvl-PS <christian.vieyra@priceshoes.com>
2025-01-23 10:09:48 -0600

    App TODOS en React.js

---
+++ src/TodoContext/useLocalStorage.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+function useLocalStorage(itemName, initialValue) {
+    const [item, setItem] = React.useState(initialValue);
+    const [loading, setLoading] = React.useState(true);
+    const [error, setError] = React.useState(false);
+
+    React.useEffect(() => {
+        setTimeout(() => {
+            try {
+                const localStorageItem = localStorage.getItem(itemName);
+
+                let parsedItem;
+
+                if (!localStorageItem) {
+                    localStorage.setItem(itemName, JSON.stringify(initialValue));
+                    parsedItem = initialValue;
+                } else {
+                    parsedItem = JSON.parse(localStorageItem);
+                    setItem(parsedItem);
+                }
+                setLoading(false);
+            } catch (error) {
+                setLoading(false);
+                setError(true);
+            }
+        }, 2000);
+    }, [initialValue, itemName]);
+
+    const saveItem = (newItem) => {
+        localStorage.setItem(itemName, JSON.stringify(newItem));
+        setItem(newItem);
+    };
+
+    return {
+        item,
+        saveItem,
+        loading,
+        error,
+    };
+}
+
+export { useLocalStorage }
+
+// const defaultTodos = [
+//   { text: 'Llenar plan de trabajo', completed: true },
+//   { text: 'Tomar el Curso de Intro a React.js', completed: false },
+//   { text: 'Comprar regalos navidad', completed: false },
+//   { text: 'Mantenimiento PC', completed: false },
+//   { text: 'Configurar app SIE', completed: true },
+// ];
+
+// localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
+// localStorage.removeItem('TODOS_V1');
---
+++ src/TodosError/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import './TodosError.css';
+
+function TodosError() {
+    return (
+        <p>¡Error!</p>
+    );
+}
+
+export { TodosError };
